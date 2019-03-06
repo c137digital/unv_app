ba915b03f48c1a55fd52851c8ec97c03cf9edf86
Morty Space <morty.space@gmail.com>
2019-03-06 16:34:03 +0200

    Ignore missing component app file [not required]

--- src/unv/app/helpers.py
+++ src/unv/app/helpers.py
@@ -8,7 +8,10 @@ from .settings import SETTINGS
 def get_app_components():
     for component in SETTINGS['components']:
         component = '{}.app'.format(component)
-        yield importlib.import_module(component)
+        try:
+            yield importlib.import_module(component)
+        except ModuleNotFoundError:
+            continue
 
 
 def get_project_root(app_module: str = 'app.settings') -> pathlib.Path:
